77859b92b80f0f0c3606bc5e8faaa65dafcb1471
khushi varshney <anuragkhushi703@gmail.com>
2024-12-10 22:39:01 +0530

    Assignment 1 done

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log(x);
+var x =10;
+console.log(x);
+x=20;
+console.log(x)
+var x= 30;
+console.log(x);
+
+// console.log(y); can't  be done with let
+let y =10;
+console.log(y);
+y=20;
+console.log(y)
+// let y= 30;
+
+const z=10;
+console.log(z)
+// z=0;
+// console.log(z);
+
+let str=`Hello world ${x}`;
+let int=10;
+let float=2.21;
+let boolean= true;
+let udefined;
+let nulll=null;
+
+let data={
+    name:"radhe",
+    isTrue: true,
+}
+
+let colors =[1, "red", {radhe : "krishna"}]
+function print(message){
+    return `Hello, ${message}`;
+}
+let prints = print('World Js')
